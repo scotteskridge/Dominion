b359bbcc2c4cd25c91ce9e7b611b3865d6c74820
Andrew Kim <andykim@Andrews-MacBook-Pro.local>
2017-04-05 12:42:11 -0700

    Andy's first commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,47 @@ namespace Dominion_Project{
         public static void Main(string[] args)
         {
             Console.WriteLine("Lets Start a new game of Dominion!");
-            GetUserString();
+            Player player1 = new Player("Scott");
+            Player player2 = new Player("Andy");
+            bool gameOn =  true;
+            int switchPlayer = 1;
+            while(gameOn == true){
+                if(switchPlayer == 1){
+                    Console.WriteLine("This is player 1 turn");
+                    bool turnOn = true;
+                    int switchPhase = 1;
+                    while(turnOn){
+                        if(switchPhase == 1){
+                            Console.WriteLine("This is Action Phase");
+                            switchPhase = 2;
+                        }else if(switchPhase == 2){
+                            Console.WriteLine("This is buy pahse");
+                            switchPhase = 3;
+                        }else if(switchPhase == 3){
+                            Console.WriteLine("This is clean Phase");
+                            turnOn = false;
+                        }
+                    }
+                    switchPlayer = 2;
+                }else if(switchPlayer == 2){
+                    Console.WriteLine("This is player 2 turn");
+                    bool turnOn = true;
+                    int switchPhase = 1;
+                    while(turnOn){
+                        if(switchPhase == 1){
+                            Console.WriteLine("This is Action Phase");
+                            switchPhase = 2;
+                        }else if(switchPhase == 2){
+                            Console.WriteLine("This is buy pahse");
+                            switchPhase = 3;
+                        }else if(switchPhase == 3){
+                            Console.WriteLine("This is clean Phase");
+                            turnOn = false;
+                        }
+                    }
+                    gameOn = false;
+                }
+            }
         }
 
         public static string GetUserString(){
